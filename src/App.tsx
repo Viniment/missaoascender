f8b085d738fff231b0a86dfe90e605fc68a18844
Viniment <viniouverney@gmail.com>
2026-09-11 17:35:03 -0300

    feat(diario): add daily campaign diary route

--- src/App.tsx
+++ src/App.tsx
@@ -14,10 +14,8 @@ import PerfilPage from "@/pages/Perfil";
 import LojaPage from "@/pages/Loja";
 import PersonalizarPage from "@/pages/Personalizar";
 import AdminPage from "@/pages/Admin";
-import Fissura from "@/pages/Fissura";
 import Reestruturacao from "@/pages/Reestruturacao";
 import UrgeSurfing from "@/pages/UrgeSurfing";
-import FissuraPage from "@/pages/Fissura";
 import Laboratorio from "@/pages/Laboratorio";
 import Predicao from "@/pages/Predicao";
 import Mente from "@/pages/Mente";
@@ -41,34 +39,35 @@ export default function App() {
     <QueryClientProvider client={qc}>
       <AuthProvider>
         <LowPowerProvider>
-        <BrowserRouter>
-          <Toaster theme="dark" position="top-center" richColors />
-          <Routes>
-            <Route path="/auth" element={<Auth />} />
-            <Route path="/onboarding" element={<Protected><Onboarding /></Protected>} />
-            <Route path="/criar-inimigo" element={<Protected><CriarInimigo /></Protected>} />
-            <Route path="/" element={<Protected><Dashboard /></Protected>} />
-            <Route path="/inimigo" element={<Protected><InimigoPage /></Protected>} />
-            <Route path="/mini-vitorias" element={<Protected><MiniVitoriasPage /></Protected>} />
-            <Route path="/conquistas" element={<Protected><ConquistasPage /></Protected>} />
-            <Route path="/fissura" element={<Protected><FissuraPage /></Protected>} />
-            <Route path="/loja" element={<Protected><LojaPage /></Protected>} />
-            <Route path="/personalizar" element={<Protected><PersonalizarPage /></Protected>} />
-            <Route path="/perfil" element={<Protected><PerfilPage /></Protected>} />
-            <Route path="/admin" element={<Protected><AdminPage /></Protected>} />
-            <Route path="/reestruturacao" element={<Protected><Reestruturacao /></Protected>} />
-            <Route path="/urge-surfing" element={<Protected><UrgeSurfing /></Protected>} />
-            <Route path="/laboratorio" element={<Protected><Laboratorio /></Protected>} />
-            <Route path="/predicao" element={<Protected><Predicao /></Protected>} />
-            <Route path="/mente" element={<Protected><Mente /></Protected>} />
-            <Route path="/trataka" element={<Protected><Trataka /></Protected>} />
-            <Route path="/japamala" element={<Protected><Japamala /></Protected>} />
-            <Route path="/estudos" element={<Protected><Estudos /></Protected>} />
-            <Route path="/estudos/:id" element={<Protected><EstudoCategoria /></Protected>} />
-            <Route path="/imersao" element={<Protected><Imersao /></Protected>} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </BrowserRouter>
+          <BrowserRouter>
+            <Toaster theme="dark" position="top-center" richColors />
+            <Routes>
+              <Route path="/auth" element={<Auth />} />
+              <Route path="/onboarding" element={<Protected><Onboarding /></Protected>} />
+              <Route path="/criar-inimigo" element={<Protected><CriarInimigo /></Protected>} />
+              <Route path="/" element={<Protected><Dashboard /></Protected>} />
+              <Route path="/inimigo" element={<Protected><InimigoPage /></Protected>} />
+              <Route path="/mini-vitorias" element={<Protected><MiniVitoriasPage /></Protected>} />
+              <Route path="/conquistas" element={<Protected><ConquistasPage /></Protected>} />
+              <Route path="/fissura" element={<Protected><Imersao /></Protected>} />
+              <Route path="/loja" element={<Protected><LojaPage /></Protected>} />
+              <Route path="/personalizar" element={<Protected><PersonalizarPage /></Protected>} />
+              <Route path="/perfil" element={<Protected><PerfilPage /></Protected>} />
+              <Route path="/admin" element={<Protected><AdminPage /></Protected>} />
+              <Route path="/reestruturacao" element={<Protected><Reestruturacao /></Protected>} />
+              <Route path="/urge-surfing" element={<Protected><UrgeSurfing /></Protected>} />
+              <Route path="/laboratorio" element={<Protected><Laboratorio /></Protected>} />
+              <Route path="/predicao" element={<Protected><Predicao /></Protected>} />
+              <Route path="/mente" element={<Protected><Mente /></Protected>} />
+              <Route path="/trataka" element={<Protected><Trataka /></Protected>} />
+              <Route path="/japamala" element={<Protected><Japamala /></Protected>} />
+              <Route path="/estudos" element={<Protected><Estudos /></Protected>} />
+              <Route path="/estudos/:id" element={<Protected><EstudoCategoria /></Protected>} />
+              <Route path="/imersao" element={<Protected><Imersao /></Protected>} />
+              <Route path="/diario" element={<Protected><Imersao /></Protected>} />
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </BrowserRouter>
         </LowPowerProvider>
       </AuthProvider>
     </QueryClientProvider>
