a1d2fad2207394a51a76dbbe3e591119dbb37e78
Viniment <viniouverney@gmail.com>
2026-09-11 20:15:09 -0300

    [Novo] Adicionar Conquistas De Jejum Ao Menu

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Onboarding from "@/pages/Onboarding";
 import CriarInimigo from "@/pages/CriarInimigo";
 import InimigoPage from "@/pages/Inimigo";
 import MiniVitoriasPage from "@/pages/MiniVitorias";
-import ConquistasPage from "@/pages/Conquistas";
+import ConquistasPage from "@/pages/ConquistasComJejum";
 import PerfilPage from "@/pages/Perfil";
 import LojaPage from "@/pages/Loja";
 import PersonalizarPage from "@/pages/Personalizar";
